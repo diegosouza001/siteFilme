80b7b77d4219873c38e62b8f7ac860e4b2ca427e
diegosouza001 <168775913+diegosouza001@users.noreply.github.com>
2025-12-01 19:36:25 -0300

    A

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,12 @@
-// Arquivo: ../services/api.js CORRIGIDO
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL || "https://sitefilme-1.onrender.com",
+  baseURL: process.env.REACT_APP_BACKEND_URL || "https://sitefilme-1.onrender.com",
 });
-
-// --- NOVO: INTERCEPTOR PARA INJETAR O TOKEN ---
-api.interceptors.request.use(async (config) => {
-  // 1. Tenta obter o token do armazenamento local
-  const token = localStorage.getItem('token');
-  
-  if (token) {
-    // 2. Se o token existir, adiciona o cabeçalho Authorization
-    // O formato "Bearer <token>" é o padrão para JWT
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  
-  return config;
-});
-// ----------------------------------------------------
+console.log("BACKEND URL:", process.env.REACT_APP_BACKEND_URL);
 
 export default api;
+
+
+
+
