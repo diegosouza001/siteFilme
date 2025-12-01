eb9ae38cad613d287ce7a3adc650d6a2b77c34e8
diegosouza001 <168775913+diegosouza001@users.noreply.github.com>
2025-12-01 10:32:07 -0300

    Ajustes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL || "https://sitefilme-1.onrender.com",
 });
-
 console.log("BACKEND URL:", process.env.REACT_APP_BACKEND_URL);
 
-
 export default api;
+
+
+
+
